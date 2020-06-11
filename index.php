1d5ca0a158d4aaa76ed6d496392976a0053f8064
Vimal <vimalvinith@rediffmail.com>
2020-06-11 08:44:56 -0400

    Revert "reverted"
    This reverts commit 6850625b6642812bdf1ad46f95cdf5cff30e2831.

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php 
-<<<<<<< HEAD
 // COmment added
 // Color test
-<<<<<<< HEAD
-=======
->>>>>>> parent of 1f6a608... first comment added
-=======
->>>>>>> be8e96b31843dfc8b9e307a84a9df0362c63d829
 phpinfo();
-//lego comment
 ?>
